82c831b56aba871eb2cfb643de1f35e33dbbc64a
rongbo <842549829@qq.com>
2017-06-06 11:17:05 +0800

    添加项目文件。

---
+++ WebUploader/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddFile(string id, string name, string type, string lastModifiedDate, int size, HttpPostedFileBase file)
+        {
+            // 处理图片存储
+            return Json(new { state = "success", message = "文件地址" });
+        }
+
+        public ActionResult DeleteFile(string fullName)
+        {
+            return Json(new { state = "success", message = "删除成功!" });
+        }
+    }
+}
